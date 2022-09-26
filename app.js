5b8a758db0686591f3e923a2a57c94916a948291
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-09-26 15:31:51 +0800

    Add alert for keyword not found

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
+const alert = require('alert')
+const restaurants = require('./restaurant.json').results //Include restaurants data from JSON file
 
-const restaurantsList = require('./restaurant.json').results //Include restaurants data from JSON file
 // Setting view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -16,12 +17,12 @@ app.use(express.static('public'))
 ////  Setting route
 // http://localhost:3000/ => index page
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantsList })
+  res.render('index', { restaurants })
 })
 
 // http://localhost0/restaurants/:id => show page
 app.get('/restaurants/:id', (req, res) => {
-  const restaurant = restaurantsList.find(
+  const restaurant = restaurants.find(
     (restaurant) => restaurant.id.toString() === req.params.id
   )
   res.render('show', { restaurant })
@@ -29,13 +30,19 @@ app.get('/restaurants/:id', (req, res) => {
 
 // http://localhost:3000/search => 搜尋功能
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.trim()
-  const filterRestaurants = restaurantsList.filter((restaurant) => {
+  let keyword = req.query.keyword.trim()
+  const filterRestaurants = restaurants.filter((restaurant) => {
     return (
       restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
       restaurant.category.toLowerCase().includes(keyword.toLowerCase())
     )
   })
+  //若filterRestaurants是undefined或null，或是長度為0，表示找不到關鍵字
+  if (!filterRestaurants || !filterRestaurants.length) {
+    alert(`Oops!找不到您要的餐廳\n再試試別的關鍵字吧!\n\n\n按enter回所有清單`)
+    keyword = ''
+  }
+
   res.render('index', { restaurants: filterRestaurants, keyword })
 })
 
