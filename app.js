43015d6b18f1da5f62266fe74f47bc9662a8885b
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-10-31 10:40:42 +0800

    feat: add login and register page

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+if (process.env.Node_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const routes = require('./routes')
 require('./config/mongoose')
