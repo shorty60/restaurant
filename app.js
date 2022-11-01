7cb1681a4c56eb72858eb9fe8425546c1c8d82eb
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-11-01 13:35:24 +0800

    feat: add FB login

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 // Setting view engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const FacebookStrategy = require('passport-facebook')
 const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
@@ -40,6 +41,35 @@ module.exports = app => {
             console.log(error)
             return done(error, false)
           })
+      }
+    )
+  )
+  // FB OAuth
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL:  process.env.FACEBOOK_CALLBACK,
+        profileFields: ['email', 'displayName'],
+      },
+      (accessToken, refreshToken, profile, done) => {
+        let { name, email } = profile._json
+        if (!name) {
+          name = 'User' //若抓不到使用者名稱，填一個值
+        }
+        User.findOne({ email }).then(user => {
+          if (user) {
+            return done(null, user)
+          }
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash => User.create({ name, email, password: hash }))
+            .then(user => done(null, user))
+            .catch(error => done(error, false))
+        })
       }
     )
   )
